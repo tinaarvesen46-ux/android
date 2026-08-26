756412afd195bc8ca32b1977355d8dfab28eb389
tinaarvesen46-ux <tinaarvesen46@gmail.com>
2026-08-26 16:04:30 +0200

    here

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "app.VibeChat"
+    namespace = "app.SwiftSnap"
     compileSdk = 36
     ndkVersion = "28.2.13676358"
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "app.VibeChat"
+        applicationId = "app.SwiftSnap"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26 // Android 8.0
